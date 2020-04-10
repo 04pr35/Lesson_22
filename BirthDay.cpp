136932f808532be033574acad2d972ed413b8b24
knvlvr <roman.konov03@gmail.com>
2020-04-10 18:18:25 +0300

    Konovalov Birthday

---
+++ BirthDay.cpp
@@ -0,0 +1,74 @@
+#include "BirthDay.h"
+
+
+
+BirthDay::BirthDay()
+{
+	hero = "none";
+	place = "none";
+	age = 0;
+}
+
+BirthDay::BirthDay(string hero, string place, int age)
+{
+	this->hero = hero;
+	this->place = place;
+	this->age = age;
+}
+
+
+BirthDay::~BirthDay()
+{
+}
+
+void BirthDay::setHero(string hero)
+{
+	if(hero.empty())
+		throw "Hero не может быть пустым";
+	this->hero = hero;
+}
+
+string BirthDay::getHero()
+{
+	return hero;
+}
+
+void BirthDay::setPlace(string place)
+{
+	if (place.empty())
+		throw "Hero не может быть пустым";
+	this->place = place;
+}
+
+string BirthDay::getPlace()
+{
+	return place;
+}
+
+void BirthDay::setAge(int age)
+{
+	if (age < 0)
+		throw "Возраст не может быть отрицательным";
+	this->age = age;
+}
+
+int BirthDay::getAge()
+{
+	return age;
+}
+
+string BirthDay::type()
+{
+	return "Birthday";
+}
+
+string BirthDay::toString()
+{
+	return "{date: " + to_string(date.getDay()) + "-" + to_string(date.getMonth()) + "-" + to_string(date.getYear()) +
+		"; time:" + to_string(time.getHour()) + "," + to_string(time.getMinutes()) + "; hero:" + hero + " " + place + to_string(age) + "}";
+}
+
+void BirthDay::show()
+{
+	cout << this->toString() << endl;
+}
